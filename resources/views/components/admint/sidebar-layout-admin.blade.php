25a1389ac05b208c48e245d034f64ac76b91ca21
younot34 <94425967+younot34@users.noreply.github.com>
2025-02-06 00:11:13 +0700

    ketinggalan 1

--- resources/views/components/admint/sidebar-layout-admin.blade.php
+++ resources/views/components/admint/sidebar-layout-admin.blade.php
@@ -51,7 +51,7 @@
                                     <a href="{{route('adm.karyawan')}}" class="nav-link @if($secondMenu == "karyawan") active @endif" data-key="t-basic-elements">Karyawan</a>
                                 </li> --}}
                                 <li class="nav-item">
-                                    <a href="{{route('adm.dokter')}}" class="nav-link @if($secondMenu == "dokter") active @endif" data-key="t-form-select"> Dokter </a>
+                                    <a href="{{route('adm.dokter')}}" class="nav-link @if($secondMenu == "dokter") active @endif" data-key="t-form-select"> Fisioterapi </a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link menu-link @if($firstMenu == 'jadwal') active @endif" href="{{ route('adm.jadwal.index') }}">
